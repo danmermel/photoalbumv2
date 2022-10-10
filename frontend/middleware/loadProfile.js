a336cd489f362aa409262a8e2141b1ee48115f76
Glynn Bird <glynn.bird@gmail.com>
2022-10-10 13:14:27 +0100

    stop logging the profile

--- frontend/middleware/loadProfile.js
+++ frontend/middleware/loadProfile.js
@@ -3,6 +3,5 @@ import localstorage from "~/assets/js/localstorage";
 export default function ({store }) {
   //get the profile from local storage and put it in the store
   const profile = localstorage.loadProfile()
-  console.log('middleware profile', profile)
   store.commit("profile/save", profile);
 }
