767f856323e33ab2a774686bed8ea4be23cae0c6
Glynn Bird <glynn.bird@gmail.com>
2022-10-11 15:28:50 +0100

    presign urls for 7 days (from 15 mins)

--- lambda/singlealbum.js
+++ lambda/singlealbum.js
@@ -9,12 +9,12 @@ const API_KEY = process.env.API_KEY;
 
 exports.handler = async function (spec) {
 
-  let albums;  
+  let albums;
   // first check if the API KEY is correct
   if (!spec.queryStringParameters || !spec.queryStringParameters.apikey || spec.queryStringParameters.apikey !== API_KEY) {
     return { statusCode: 401, body: '{"ok": false}' }
   }
-  
+
   // get query string parameters
   const album = spec.queryStringParameters.album
   if (!album) {
@@ -33,25 +33,23 @@ exports.handler = async function (spec) {
       MaxKeys: 20
     }
     data = await s3.listObjects(params).promise()
-    
-    for(var i = 0; i < data.Contents.length ; i++) {
+
+    for (var i = 0; i < data.Contents.length; i++) {
       const key = data.Contents[i].Key
-      const url = await s3.getSignedUrlPromise('getObject', {Bucket: BUCKET, Key: key})
+      // create presigned url that allows object to be fetched for 7 days
+      const opts = { Bucket: BUCKET, Key: key, Expires: 60 * 60 * 24 * 7 }
+      const url = await s3.getSignedUrlPromise('getObject', opts)
       const obj = {
         key,
         url
       }
       retval.push(obj)
     }
-    console.log("data is ", data)
-    // albums = data.CommonPrefixes.map(function (commonPrefix) {
-    //   return decodeURIComponent(commonPrefix.Prefix.replace('/', ''));
-    // })
   } catch (e) {
     console.log('ERROR!', e)
     return { statusCode: 500, body: '{"ok": false}' }
 
   }
-  return { statusCode: 200, body: JSON.stringify({"ok": true, images: retval, endReached: !data.IsTruncated}) }
+  return { statusCode: 200, body: JSON.stringify({ "ok": true, images: retval, endReached: !data.IsTruncated }) }
 
 }
