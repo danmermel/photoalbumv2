c2f7cccf1e492d16aba3d26d779d2e732c0d2fe8
Glynn Bird <glynn.bird@gmail.com>
2023-03-10 10:58:47 +0000

    remove RSS links

--- frontend/components/NavDrawer.vue
+++ frontend/components/NavDrawer.vue
@@ -9,20 +9,6 @@
         <v-list-item-title>Home</v-list-item-title>
       </v-list-item>
 
-      <v-list-item v-if="this.$store.state.profile.profile" @click="onFeedList">
-        <v-list-item-icon>
-          <v-icon>mdi-format-list-bulleted</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>List Feeds</v-list-item-title>
-      </v-list-item>
-
-      <v-list-item v-if="this.$store.state.profile.profile" @click="onFeedAdd">
-        <v-list-item-icon>
-          <v-icon>mdi-rss</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Add Feed</v-list-item-title>
-      </v-list-item>
-
       <v-divider v-if="this.$store.state.profile.profile"></v-divider>
 
       <v-list-item v-if="this.$store.state.profile.profile" @click="onLogout">
